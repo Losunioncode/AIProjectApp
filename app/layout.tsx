accfe000616152d2a4ae57261aaf212afd4abd10
Losunioncode <klim.lipkin45@gmail.com>
2024-03-07 18:30:12 +0100

    alpha v1.1.6

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,9 @@ import React from "react";
 import type { Metadata } from "next";
 import { Gabarito } from "next/font/google";
 import { Toaster } from "@/components/ui/toaster";
-import { sessionClient } from "./lib/sessionClientUser";
 import ReduxProvider from "./ProviderRedux/ReduxProvider";
 
-import { Provider } from "react-redux";
 import "./globals.css";
-import { useEffect } from "react";
-const authtenticated = sessionClient();
 
 const inter = Gabarito({ subsets: ["latin"] });
 
